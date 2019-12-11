46989167ebcde15c4ca934edd95a9660e93d99c1
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 19:45:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+# my_collect(array) do  |element|
+#   element.split
+i = 0
+  collection = []
+  while i < array.length
+  
+  collection << yield(array[i])
+    i += 1
+  end
+  collection 
+end
+
 
 
