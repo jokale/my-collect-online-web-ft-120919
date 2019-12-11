c30e65882a1426980e27a803b9a2313d86fad2f2
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 19:45:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,15 @@
 def my_collect(array)
 # my_collect(array) do  |element|
 #   element.split
+i = 0
+  collection = []
+  while i < array.length
+  
+  collection << yield(array[i])
+    i += 1
+  end
+  collection 
 end
-end 
-
+end
 
 
